b945f09cfbf3e47130d4852de2e96211e7b4aa3e
Fufutis <fufutis15@gmail.com>
2024-12-06 11:11:18 +0200

    General structure i want to follow

--- WebsiteProject/index.php
+++ WebsiteProject/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include("header.php"); ?>
+<?php include("/repeat/header.php"); ?>
 
 <body>
     <?php include("login-page.php"); ?>
---
+++ WebsiteProject/repeat/config.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'final';
+
+$conn = new mysqli($host, $db_user, $db_pass, $db_name);
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
