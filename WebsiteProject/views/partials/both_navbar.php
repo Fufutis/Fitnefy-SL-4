8a9f06da566fe3664815ce3b497b25f718a5ead9
Fufutis <fufutis15@gmail.com>
2024-12-18 23:36:56 +0200

    navbars fix

--- WebsiteProject/views/partials/both_navbar.php
+++ WebsiteProject/views/partials/both_navbar.php
@@ -1,20 +1,20 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="dashboard.php">Both Dashboard</a>
+        <a class="navbar-brand" href="<?php echo BASE_URL; ?>/controllers/dashboard.php">Both Dashboard</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto">
-                <li class="nav-item"><a class="nav-link" href="seller_store.php">Products</a></li>
-                <li class="nav-item"><a class="nav-link" href="wishlist_view.php">Wishlist</a></li>
-                <li class="nav-item"><a class="nav-link" href="order_history.php">Order History</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo BASE_URL; ?>/views/seller_store.php">Products</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo BASE_URL; ?>/views/wishlist_view.php">Wishlist</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo BASE_URL; ?>/views/order_history.php">Order History</a></li>
             </ul>
             <ul class="navbar-nav">
-                <li class="nav-item"><a class="btn btn-light" href="cart_view.php">Cart</a></li>
-                <li class="nav-item"><a class="btn btn-light" href="seller.php">Sell</a></li>
-                <li class="nav-item"><a class="btn btn-danger" href="logout.php">Logout</a></li>
+                <li class="nav-item"><a class="btn btn-light" href="<?php echo BASE_URL; ?>/views/cart_view.php">Cart</a></li>
+                <li class="nav-item"><a class="btn btn-light" href="<?php echo BASE_URL; ?>/views/seller.php">Sell</a></li>
+                <li class="nav-item"><a class="btn btn-danger" href="<?php echo BASE_URL; ?>/controllers/logout.php">Logout</a></li>
             </ul>
         </div>
     </div>
