4d60386f969f5fc9749f87ca42835206223053a9
Fufutis <fufutis15@gmail.com>
2025-01-05 17:11:55 +0200

    selling and nav color changed

--- WebsiteProject/repeat/navbar.php
+++ WebsiteProject/repeat/navbar.php
@@ -4,9 +4,9 @@ $role = $_SESSION['role'] ?? 'user'; // Default role is 'user'
 // Function to display the User Navbar
 function displayUserNavbar()
 {
-    ?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <div class="container">
+?>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top sheet">
+        <div class="container sheet">
             <a class="navbar-brand" href="dashboard.php">User Dashboard</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,26 +14,26 @@ function displayUserNavbar()
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav me-auto">
-                    <li class="nav-item"><a class="nav-link" href="seller_store.php">Products</a></li>
+                    <li class="nav-item "><a class="nav-link" href="seller_store.php">Products</a></li>
                     <li class="nav-item"><a class="nav-link" href="wishlist_view.php">Wishlist</a></li>
                     <li class="nav-item"><a class="nav-link" href="order_history.php">Order History</a></li>
                 </ul>
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="btn btn-light" href="cart_view.php">Cart</a></li>
-                    <li class="nav-item"><a class="btn  " ></a></li>
+                    <li class="nav-item"><a class="btn  "></a></li>
                     <li class="nav-item"><a class="btn btn-danger" href="logout.php">Logout</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-    <?php
+<?php
 }
 
 // Function to display the Seller Navbar
 function displaySellerNavbar()
 {
-    ?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+?>
+    <nav class="navbar navbar-expand-lg navbar-dark  fixed-top sheet">
         <div class="container">
             <a class="navbar-brand" href="dashboard.php">Seller Dashboard</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-toggle="navbarNav"
@@ -47,20 +47,20 @@ function displaySellerNavbar()
                 </ul>
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="btn btn-light" href="seller.php">Sell</a></li>
-                    <li class="nav-item"><a class="btn  " ></a></li>
+                    <li class="nav-item"><a class="btn  "></a></li>
                     <li class="nav-item"><a class="btn btn-danger" href="logout.php">Logout</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-    <?php
+<?php
 }
 
 // Function to display the Both User and Seller Navbar
 function displayBothNavbar()
 {
-    ?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+?>
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top sheet">
         <div class="container">
             <a class="navbar-brand" href="dashboard.php">Both Dashboard</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -74,17 +74,17 @@ function displayBothNavbar()
                     <li class="nav-item"><a class="nav-link" href="order_history.php">Order History</a></li>
                 </ul>
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="btn btn-light" href="cart_view.php">Cart</a></li>
-                    <li class="nav-item"><a class="btn  " ></a></li>
-                    <li class="nav-item"><a class="btn btn-light" href="seller.php">Sell</a></li>
-                    <li class="nav-item"><a class="btn  " ></a></li>
+                    <li class="nav-item"><a class="btn bg-item" href="cart_view.php">Cart</a></li>
+                    <li class="nav-item"><a class="btn  "></a></li>
+                    <li class="nav-item"><a class="btn bg-item" href="seller.php">Sell</a></li>
+                    <li class="nav-item"><a class="btn  "></a></li>
 
                     <li class="nav-item"><a class="btn btn-danger" href="logout.php">Logout</a></li>
                 </ul>
             </div>
         </div>
     </nav>
-    <?php
+<?php
 }
 
 // Display the appropriate navbar based on the user's role
